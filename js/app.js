b427cb9ff446b113519bd696aed257f2aff49d01
Stollie <remco.raaymakers@gmail.com>
2013-04-16 16:55:28 +0200

    Soo close

--- js/app.js
+++ js/app.js
@@ -1,11 +1,61 @@
-App = Ember.Application.create();
+$(function() {  
+    App = Ember.Application.create({
+    name: "Motoriek"});
 
-App.Router.map(function() {
-  // put your routes here
-});
+    /*
+     * Controllers
+     */
+    App.ExerciseController = Ember.Controller.extend();
+    App.MotionlogController = Ember.Controller.extend();
+    
+    
+    /*App.Router.map(function() {
+        this.resources('motionlogs', {path:'motionlogs/:log_id'});
+    });*/
+    
+    /*
+     * Modals
+     
+    App.Motionlog = DS.Model.extend({
+        xyz: DS.attr('number')
+    });
+    
+    App.Exercise = DS.Modal.extend({
+        name: DS.attr('string')
+    });*/
+    
+    App.Motionlog = Ember.Object.extend({
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
+    });
+    /*App.Exercise = Ember.Object.extend({
+
+    });*/
+    
+    /*
+     * Views
+     */
+    App.MotionlogView = Ember.View.extend({
+      // the controller is the initial context for the template
+      controller: null,
+      template: Ember.Handlebars.compile("<h3>{{xyz}}</h3>")
+    });
+
+    var motionlog = App.Motionlog.create();
+    var motionlogController = App.MotionlogController.create({ content: motionlog });
+
+    App.MotionlogView.create({ controller: motionlogController }).appendTo('body');
+
+    jQuery.getJSON("api/v1/logs/3", function(json) {
+      motionlog.setProperties(json);
+    });
+
+//    App.IndexRoute = Ember.Route.extend({
+//      model: function() {
+//        return ['red', 'yellow', 'blue'];
+//      }
+//    });
+
+    
+    
+//    App.initialize();
 });
