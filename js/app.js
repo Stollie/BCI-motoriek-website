b2c53670864a3f5297d3fab1e7688f6d6a199cc1
Stollie <remco.raaymakers@gmail.com>
2013-04-17 10:10:47 +0200

    Api wordt uitgelezen en weergegeven in een lijst.

--- js/app.js
+++ js/app.js
@@ -1,61 +1,33 @@
 $(function() {  
     App = Ember.Application.create({
     name: "Motoriek"});
+    
+    // Maak een object
+    App.Motionlog = Ember.Object.extend({});
+    /*App.Exercise = Ember.Object.extend({
 
-    /*
-     * Controllers
-     */
-    App.ExerciseController = Ember.Controller.extend();
-    App.MotionlogController = Ember.Controller.extend();
-    
-    
+    });*/    
+
+    App.MotionlogsController = Ember.ArrayController.create();
+   
     /*App.Router.map(function() {
         this.resources('motionlogs', {path:'motionlogs/:log_id'});
     });*/
-    
-    /*
-     * Modals
-     
-    App.Motionlog = DS.Model.extend({
-        xyz: DS.attr('number')
-    });
-    
-    App.Exercise = DS.Modal.extend({
-        name: DS.attr('string')
-    });*/
-    
-    App.Motionlog = Ember.Object.extend({
-
-    });
-    /*App.Exercise = Ember.Object.extend({
-
-    });*/
-    
+        
     /*
      * Views
      */
-    App.MotionlogView = Ember.View.extend({
-      // the controller is the initial context for the template
-      controller: null,
-      template: Ember.Handlebars.compile("<h3>{{xyz}}</h3>")
+   /* App.MotionlogsView = Ember.CollectionView.create({
+        classNames: ['log-item'],
+        // the controller is the initial context for the template
+        controller: null,
+        itemViewClass: Ember.View.extend({
+            template: Ember.Handlebars.compile('<p>{{view.id}} {{view.xyz}}</p>')
+        })
+    });*/
+
+    jQuery.getJSON("api/v1/logs/", function(json) {
+        App.MotionlogsController.set('content', json)
     });
 
-    var motionlog = App.Motionlog.create();
-    var motionlogController = App.MotionlogController.create({ content: motionlog });
-
-    App.MotionlogView.create({ controller: motionlogController }).appendTo('body');
-
-    jQuery.getJSON("api/v1/logs/3", function(json) {
-      motionlog.setProperties(json);
-    });
-
-//    App.IndexRoute = Ember.Route.extend({
-//      model: function() {
-//        return ['red', 'yellow', 'blue'];
-//      }
-//    });
-
-    
-    
-//    App.initialize();
 });
